4d4b356c5e494ee338c7aab8be0026a20863ce25
Faoi0 <Faoi0@DESKTOP-SVSR5MM>
2024-06-02 22:17:19 +0900

    [Finish]chapter1

---
+++ JavaIntroduction/src/chapter1/Chapter1_exercise.java
@@ -0,0 +1,20 @@
+package chapter1;
+
+public class Chapter1_exercise {
+	public static void main(String[] args) {
+		//練習問題1-2
+		int a =3;
+		int b = 5;
+		int c = a * b;
+		System.out.println("縦幅3横幅5の長方形の面積は、" + c);
+		
+		//練習問題1-3
+		boolean result = true;
+		char kanji = '駆';
+		double pi = 3.14;
+		long mas = 314159265853979L;
+		String statement = "ミナトの攻撃！艇に15ポイントのダメージを与えた。";
+		
+	}
+
+}
