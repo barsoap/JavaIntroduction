e738e6c862609a948f1fd15c92adcab5b65f31bc
Faoi0 <Faoi0@DESKTOP-SVSR5MM>
2024-06-02 20:15:28 +0900

    [practice]1-0

---
+++ JavaIntroduction/src/chapter1/Chapter1_0.java
@@ -0,0 +1,28 @@
+package chapter1;
+
+public class Chapter1_0 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// p22
+		System.out.println("菅原拓真です。");
+		System.out.println("31歳です。");
+		System.out.println("お酒が好きです。");
+		
+		//p23
+		System.out.println("31 + 31の計算をします。");
+		System.out.println(31 + 31);
+		
+		//p24
+		System.out.println(35 - 10);
+		System.out.println(-5 * 2);
+		System.out.println(6 * 6 * 3.14);
+		System.out.println("こたえは" + 64);
+		
+		//p25
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
+	}
+
+}
