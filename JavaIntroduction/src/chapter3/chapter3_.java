0f56b7f897ce511899f80d2a9e8857d04760277c
Faoi0 <Faoi0@DESKTOP-SVSR5MM>
2024-06-11 22:38:06 +0900

    [Add]Fortune

--- JavaIntroduction/src/chapter3/chapter3_.java
+++ JavaIntroduction/src/chapter3/chapter3_.java
@@ -56,7 +56,22 @@ public class chapter3_ {
 		// 短絡評価
 		
 		//p115　分岐構文のバリエーション
-		//
+		//if文 else if
+		
+		//p118
+		//switch文による分岐
+		System.out.println("あなたの運勢を占います");
+		int fortune = new java.util.Random().nextInt(4) + 1;
+		System.out.println(fortune);
+		if (fortune == 1) {
+			System.out.println("大吉");
+		} else if (fortune == 2) {
+			System.out.println("中吉");
+		} else if (fortune == 3) {
+			System.out.println("吉");
+		} else {
+			System.out.println("凶");
+		}
 		
 	}
 
